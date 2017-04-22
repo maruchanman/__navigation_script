d65aa7e555c354e5616dab58c924d1bdcaa48bf7
guruttosekai2011 <guruttosekai2011@gmail.com>
2017-04-23 02:25:40 +0900

    util.pyの表記統一

--- api/modules/utils.py
+++ api/modules/utils.py
@@ -1,9 +1,13 @@
-# coding-utf-8
+# -*- coding:utf-8 -*-
+# Author: Yuta Yamashita
 
 import json
 
 
 def load_json(path):
+    '''
+    Loading json file.
+    '''
     with open(path, "r") as f:
         return json.load(f)
 
@@ -14,6 +18,5 @@ def sorted_by_dictkey(target, sort_key):
     [{id:2, content:"bbb"}, {id:1, content:"aaa"}]
     -> [{id:1, content:"aaa"}, {id:2, content:"bbb"}]
     '''
-
     sorted_list = sorted(target, key=lambda x:x[sort_key])
     return sorted_list
