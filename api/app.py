49dbcbefe78cede042078d009b0a856237cefb1a
hacker <hacker@pc-2.local>
2017-04-23 16:33:01 +0900

    update app.py

--- api/app.py
+++ api/app.py
@@ -3,14 +3,17 @@ import os
 import re
 import datetime
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from modules.selector import Selector
 
 app = Flask(__name__)
+CORS(app)
 CURRENTPATH = os.path.dirname(__file__)
 
 @app.route('/navibo/<clientID>', methods=['POST'])
-def route():
-    return jsonify({"status": "ok"})
+def route(clientID):
+    selector = Selector(clientID, request.json)
+    return jsonify(selector.get_message())
 
 if __name__ == '__main__':
     app.run(port=5000)
